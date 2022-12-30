b23e94a23d48fae11ef27b66af2eb5fbdeaed07d
Digvijay1133 <112845896+Digvijay1133@users.noreply.github.com>
2022-12-30 14:32:42 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  region     = "ap-south-2"
-  access_key = "AKIAZIOD2MWJKXB2AOPT"
-  secret_key = "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"
-}
 
 # Defining CIDR Block for VPC
 variable "vpc_cidr" {
